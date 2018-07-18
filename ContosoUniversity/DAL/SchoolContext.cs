eac3bbc1be0797596aad0436f654e33cb7f088f7
Toby.Yang <y976362357@163.com>
2018-07-18 21:05:39 +0800

    feat(School):Implementing Inheritance

--- ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -25,6 +25,9 @@ namespace ContosoUniversity.DAL
         public DbSet<Student> Students { get; set; }
         public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
 
+        public DbSet<Person> People { get; set; }
+
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,43 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-
 namespace ContosoUniversity.Models
 {
-    public class Student
+    public class Student : Person
     {
-        public int ID { get; set; }
-
-        [Required]
-        [Display(Name = "Last Name")]
-        [StringLength(50,MinimumLength =1, ErrorMessage = "Last Name can not be longer than 50 characters")]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
-        public string LastName { get; set; }
-
-
-        [Required]
-        [Display(Name = "First Name")]
-        [StringLength(50, ErrorMessage = "First Name can not be longer than 50 characters")]
-        [Column("FirstName")]
-        public string FirstMidName { get; set; }
-
-
-        [Display(Name = "Enrollment Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
 
-
-        [Display(Name ="Full Name")]
-        public string FullName {
-            get {
-                return FirstMidName+", " + LastName;
-            }
-        }
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
 }
