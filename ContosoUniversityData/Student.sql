d21998498273e76e2ece6c3c4a12b4f293b1e505
Toby.Yang <y976362357@163.com>
2018-07-22 20:14:26 +0800

    feat(DB First):CotosoSite complete

---
+++ ContosoUniversityData/Student.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Student] (
+    [StudentID]      INT           IDENTITY (1, 1) NOT NULL,
+    [LastName]       NVARCHAR (50) NULL,
+    [FirstName]      NVARCHAR (50) NULL,
+	[MiddleName ] nvarchar(50) null,
+    [EnrollmentDate] DATETIME      NULL,
+    PRIMARY KEY CLUSTERED ([StudentID] ASC)
+)
